d8e79595f1caa9a19bfc8ef35e35405d19d41fb8
Alexandr <sanya.burayev.03@mail.ru>
2026-03-15 14:57:35 +0300

    feat(auth): Добавлены методы поиска пользователя и удаления токенов в JPA репозитории

---
+++ auth/src/main/java/com/vpn/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,18 @@
+package com.vpn.auth.repository;
+
+
+import com.vpn.auth.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    @Query("DELETE FROM RefreshToken rt WHERE rt.user_id = :userId")
+    void deleteByUserId(@Param("userId") Long userId);
+}
