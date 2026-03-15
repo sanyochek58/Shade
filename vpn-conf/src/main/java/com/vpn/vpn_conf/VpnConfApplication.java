b1c9da94399568b5fb27ff9689bed326893ded29
Alexandr <sanya.burayev.03@mail.ru>
2026-03-15 14:09:04 +0300

    feat(auth): initial commit

---
+++ vpn-conf/src/main/java/com/vpn/vpn_conf/VpnConfApplication.java
@@ -0,0 +1,13 @@
+package com.vpn.vpn_conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpnConfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpnConfApplication.class, args);
+	}
+
+}
